c3c5e69c12c764c6e8bb05f0d114cf184d36bfe7
Le Thanh <lethanh@Les-MacBook-Pro.local>
2019-11-03 17:04:27 +0700

    Add Firebase Auth

---
+++ src/components/login-form/index.tsx
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import { navigate } from 'gatsby';
+
+import { LoginForm as LoginFormStyle } from './styles.scss';
+
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firebase: null,
+      email: '',
+      password: '',
+      error: ''
+    };
+  }
+
+  componentDidMount() {
+    const { authService } = this.props;
+    this.setState({
+      firebase: authService.firebase
+    });
+
+  }
+
+  handleFieldChange = (field) => (e: Event) => {
+    this.setState({
+      [field]: e.target.value,
+    });
+  }
+
+  handleOnLogin = (e) => {
+    const { firebase, email, password } = this.state;
+    if (firebase) {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(data => {
+          // TODO: Shoud navigate back to last state
+          // Eg: Shoping cart
+          navigate('/');
+
+        })
+        .catch(error => {
+          console.log(error);
+          switch (error.code) {
+            case 'auth/user-not-found':
+              this.setState({
+                error: 'Not found any credential. Did you sign up?'
+              });
+              break;
+            default:
+              this.setState({
+                error: error.message
+              });
+          }
+        })
+    }
+  }
+
+  handleSignInSuccess = (e) => {
+    console.log('Sign in success');
+    console.log(e);
+  }
+
+  handleSignInFailure = (e) => {
+    console.log('Sign in failure');
+    console.log(e);
+  }
+
+  render() {
+    const { authService, currentUser } = this.props;
+    const { firebase, error, email, password } = this.state;
+    console.log(currentUser);
+    if (currentUser) {
+      return (
+        <p>Hello {currentUser.displayName}</p>
+      )
+    }
+
+    if (!firebase) {
+      return null;
+    }
+
+    return (
+      <div className={LoginFormStyle}>
+        <h3 className="center">Login</h3>
+        <div
+          className="form-field"
+        >
+          <input
+            type="text"
+            name="email"
+            placeholder="Your email"
+            value={email}
+            onChange={this.handleFieldChange('email')}
+          />
+        </div>
+
+        <div
+          className="form-field"
+        >
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={this.handleFieldChange('password')}
+          />
+        </div>
+
+        <p>
+          <a href="#">I forgot my password</a>
+        </p>
+
+        {error && <p className="error">
+          <i className="fa fa-exclamation-circle" aria-hidden="true"></i> {error}
+        </p>}
+
+        <div className="center">
+          <input type="submit" onClick={this.handleOnLogin} className="btn btn-primary btn-large" value="Next" />
+        </div>
+      </div>
+    )
+  }
+}
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,7 +1,54 @@
 import { AuthenticatedUser } from "./types";
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  staging: {
+    apiKey: 'AIzaSyCyxWY7qZSWlncB_JDYOSzeTOfRnYhNcS8',
+    authDomain: 'worldbrain-staging.firebaseapp.com',
+    databaseURL: 'https://worldbrain-staging.firebaseio.com',
+    projectId: 'worldbrain-staging',
+    messagingSenderId: '840601505816',
+    appId: '1:840601505816:web:69fbb7a789882e399fb36d',
+  },
+  // TODO: Add production keys
+  production: {
+    apiKey: 'AIzaSyCyxWY7qZSWlncB_JDYOSzeTOfRnYhNcS8',
+    authDomain: 'worldbrain-staging.firebaseapp.com',
+    databaseURL: 'https://worldbrain-staging.firebaseio.com',
+    projectId: 'worldbrain-staging',
+    messagingSenderId: '840601505816',
+    appId: '1:840601505816:web:69fbb7a789882e399fb36d',
+  },
+}
 
 export class AuthService {
-    getAuthenticatedUser(): AuthenticatedUser | null {
-        return { displayName: `John Doe` }
+  public firebase = null;
+  public user = null;
+
+  constructor() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(
+        process.env.NODE_ENV === 'production'
+          ? firebaseConfig.production
+          : firebaseConfig.staging,
+      );
+
+      this.firebase = firebase;
     }
+
+    this.firebase.auth().onAuthStateChanged((user) => {
+      console.log('Auth state change');
+      if (user) {
+        this.user = user;
+      }
+      console.log(this.user);
+    });
+
+    window.firebase = firebase;
+  }
+
+  currentUser(): AuthenticatedUser | null {
+    return this.user;
+  }
 }
