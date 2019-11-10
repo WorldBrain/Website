938fbf7f9c328a31c83956e23f5e3b244883e1e2
Le Thanh <lethanh@Les-MacBook-Pro.local>
2019-11-10 21:11:35 +0700

    Add charge bee flow

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,12 @@
 import { Services } from "./types";
 import { AuthService } from "./auth";
 import { NetworkService } from "./network";
+import { PaymentService } from "./payment";
 
 export function createServices(): Services {
     return {
         auth: new AuthService(),
-        network: new NetworkService()
+        network: new NetworkService(),
+        payment: new PaymentService(),
     }
 }
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,7 +1,16 @@
 import { AuthService } from "./auth";
 import { NetworkService } from "./network";
+import { PaymentService } from "./payment";
 
 export interface Services {
     auth: AuthService
-    network: NetworkService
+    network: NetworkService,
+    payment: PaymentService
 }
+
+export type UserPlans =
+    | 'free'
+    | 'backup-monthly'
+    | 'backup-yearly'
+    | 'sync-monthly'
+    | 'sync-yearly'
