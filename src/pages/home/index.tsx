6f543213effb040235d4b4ceff54353f0dfdd611
Le Thanh <lethanh@Les-MacBook-Pro.local>
2019-11-14 00:14:55 +0700

    Add new design

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,84 @@
-import React from 'react'
-import DefaultPageLayout from '../../components/layout/default-page-layout'
-import Page from '../../components/page'
-import Section1 from './section1'
-import Section2 from './section2'
-import Section3 from './section3'
-import Section4 from './section4'
-import Section5 from './section5'
-import Section6 from './section6'
-import Section7 from './section7'
+import React, { useState, useEffect, Fragment } from 'react';
+import { ThemeProvider } from 'styled-components';
+import Sticky from 'react-stickynode';
+import { Modal } from '@redq/reuse-modal';
+import { appTheme } from 'common/src/theme/app';
+import {
+  GlobalStyle,
+  AppWrapper,
+  ConditionWrapper,
+} from '../../containers/App/app.style';
+import { ResetCSS } from 'common/src/assets/css/style';
+import Navbar from '../../containers/App/Navbar';
+import DomainSection from '../../containers/App/Banner';
+import FeatureSection from '../../containers/App/FeatureSection';
+import ControllSection from '../../containers/App/Control';
+import TestimonialSection from '../../containers/App/Testimonial';
+import PartnerHistory from '../../containers/App/PartnerHistory';
+import PaymentSection from '../../containers/App/PaymentSection';
+import Footer from '../../containers/App/Footer';
+import FeatureSlider from '../../containers/App/FeatureSlider';
+import FeatureSliderTwo from '../../containers/App/FeatureSliderTwo';
+import { DrawerProvider } from 'common/src/contexts/DrawerContext';
+import '@redq/reuse-modal/es/index.css';
+// import SEO from '../../components/seo';
 
-export default class HomePage extends Page {
-  render() {
-    return (
-      <DefaultPageLayout pageTitle="Home">
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section5 />
-        <Section6 />
-        <Section7 />
-      </DefaultPageLayout>
-    )
-  }
+function getSize() {
+  return {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth,
+  };
 }
+
+function useWindowSize() {
+  let [windowSize, setWindowSize] = useState(getSize());
+
+  function handleResize() {
+    setWindowSize(getSize());
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowSize;
+}
+
+export default () => {
+  const size = process.browser && useWindowSize();
+  const innerWidth = process.browser && size.innerWidth;
+
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Fragment>
+        {/* <SEO title="App | A react next landing page" /> */}
+        <Modal />
+        <ResetCSS />
+        <GlobalStyle />
+        <AppWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          <DomainSection />
+          <FeatureSection />
+          <ControllSection />
+          <ConditionWrapper id="keyfeature">
+            {innerWidth > 1100 ? <FeatureSlider /> : <FeatureSliderTwo />}
+          </ConditionWrapper>
+          <PartnerHistory />
+          <PaymentSection />
+          <TestimonialSection />
+          <Footer />
+        </AppWrapper>
+      </Fragment>
+    </ThemeProvider>
+  );
+};
