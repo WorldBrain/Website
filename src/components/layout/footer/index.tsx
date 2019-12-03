5d7fe501cfe9a271bf03ecbedb497445ad6bf74d
Kelly <kellective@gmail.com>
2019-12-03 19:30:29 +1300

    Need these pages still apparently

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,119 +1,119 @@
-// import React from 'react'
-// import SubscribeForm from '../../subscribe-form'
-// import { Footer as FooterStyle } from './styles.scss';
+import React from 'react'
+import SubscribeForm from '../../subscribe-form'
+import { Footer as FooterStyle } from './styles.scss';
 
-// export default class Footer extends React.Component {
-//   render() {
-//     return (
-//       <footer className={FooterStyle}>
-//         <div className="container">
-//           <div className="row">
-//             <div className="col-md-4 col-sm-6">
-//               <SubscribeForm />
-//             </div>
-//             <div className="col-md-4 col-sm-6">
-//               <section className="widget text-3 widget_text">
-//                 <div className="widget-inner">
-//                   <div className="textwidget">
-//                     <p>
-//                       <strong>LEGAL</strong>
-//                     </p>
-//                     <p>
-//                       <a
-//                         href="https://worldbrain.io/privacy"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         Privacy Policy
-//                 </a>
-//                     </p>
-//                     <p>
-//                       <a
-//                         href="https://worldbrain.io/tos"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         Terms of Service
-//                 </a>
-//                     </p>
-//                     <p>
-//                       <a
-//                         href="https://worldbrain.io/imprint"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         Imprint
-//                 </a>
-//                     </p>
-//                   </div>
-//                 </div>
-//               </section>
-//             </div>
-//             <div className="col-md-4 col-sm-6">
-//               <section className="widget text-4 widget_text">
-//                 <div className="widget-inner">
-//                   <div className="textwidget">
-//                     <p>
-//                       <strong>PRODUCT</strong>
-//                     </p>
-//                     <p>
-//                       <a
-//                         href="https://worldbrain.io/help"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         Support
-//                 </a>
-//                     </p>
-//                     <p>
-//                       <a
-//                         href="https://worldbrain.io/roadmap"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         Roadmap
-//                 </a>
-//                     </p>
-//                     <p>
-//                       <a
-//                         href="https://github.com/worldbrain"
-//                         target="_blank"
-//                         rel="noopener noreferrer"
-//                       >
-//                         GitHub
-//                 </a>
-//                     </p>
-//                   </div>
-//                 </div>
-//               </section>
-//               <section className="widget widget-social">
-//                 <div className="widget-inner">
-//                   <div className="soc-widget">
-//                     <a target="_blank" href="https://www.facebook.com/realworldbrain">
-//                       <i className="fa fa-facebook" />
-//                     </a>
-//                     <a target="_blank" href="https://twitter.com/worldbrain">
-//                       <i className="fa fa-twitter" />
-//                     </a>
-//                     <a target="_blank" href="https://github.com/worldbrain">
-//                       <i className="fa fa-github" />
-//                     </a>
-//                   </div>
-//                 </div>
-//               </section>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="footer-btm-bar">
-//           <div className="container">
-//             <div className="footer-copyright row">
-//               <div className="col-xs-12">
-//                 <p> </p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </footer>
-//     )
-//   }
-// }
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className={FooterStyle}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-sm-6">
+              <SubscribeForm />
+            </div>
+            <div className="col-md-4 col-sm-6">
+              <section className="widget text-3 widget_text">
+                <div className="widget-inner">
+                  <div className="textwidget">
+                    <p>
+                      <strong>LEGAL</strong>
+                    </p>
+                    <p>
+                      <a
+                        href="https://worldbrain.io/privacy"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Privacy Policy
+                </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://worldbrain.io/tos"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Terms of Service
+                </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://worldbrain.io/imprint"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Imprint
+                </a>
+                    </p>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div className="col-md-4 col-sm-6">
+              <section className="widget text-4 widget_text">
+                <div className="widget-inner">
+                  <div className="textwidget">
+                    <p>
+                      <strong>PRODUCT</strong>
+                    </p>
+                    <p>
+                      <a
+                        href="https://worldbrain.io/help"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Support
+                </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://worldbrain.io/roadmap"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Roadmap
+                </a>
+                    </p>
+                    <p>
+                      <a
+                        href="https://github.com/worldbrain"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        GitHub
+                </a>
+                    </p>
+                  </div>
+                </div>
+              </section>
+              <section className="widget widget-social">
+                <div className="widget-inner">
+                  <div className="soc-widget">
+                    <a target="_blank" href="https://www.facebook.com/realworldbrain">
+                      <i className="fa fa-facebook" />
+                    </a>
+                    <a target="_blank" href="https://twitter.com/worldbrain">
+                      <i className="fa fa-twitter" />
+                    </a>
+                    <a target="_blank" href="https://github.com/worldbrain">
+                      <i className="fa fa-github" />
+                    </a>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <div className="footer-btm-bar">
+          <div className="container">
+            <div className="footer-copyright row">
+              <div className="col-xs-12">
+                <p> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,58 +1,58 @@
-// import React from 'react'
-// import { Link } from 'gatsby';
+import React from 'react'
+import { Link } from 'gatsby';
 
-// import { Header as HeaderClass } from './styles.scss';
+import { Header as HeaderClass } from './styles.scss';
 
-// export default class Header extends React.Component {
-//   handleLogOut = (e: Event) => {
-//     e.preventDefault();
-//     const { auth } = this.props;
+export default class Header extends React.Component {
+  handleLogOut = (e: Event) => {
+    e.preventDefault();
+    const { auth } = this.props;
 
-//     auth.logOut();
-//   }
+    auth.logOut();
+  }
 
-//   render() {
-//     const { user } = this.props;
+  render() {
+    const { user } = this.props;
 
-//     return (
-//       <header className={HeaderClass} role="banner">
-//         <div className="container">
-//           <div className="navbar-header">
-//             <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span> </button>
-//             <div id="logo">
-//               <Link to="/">
-//                 <img width="164" height="40" src="/images/logo_horizontal_small-164x40.png" alt="Memex by worldbrain.io" />
-//               </Link>
-//             </div>
-//           </div>
-//           <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-//             <ul id="menu-main-menu-bar" className="nav navbar-nav">
-//               <li className="dropdown menu-about"><a className="dropdown-toggle">About</a>
-//                 <ul className="dropdown-menu">
-//                   <li className="menu-jobs"><a href="https://www.notion.so/worldbrain/4d79e20aa01d4b9e8b8c12d88bb1b4ad">Jobs</a></li>
-//                   <li className="menu-faqs"><a href="/faq">FAQs</a></li>
-//                   <li className="menu-vision"><a href="https://medium.com/@WorldBrain/where-we-are-heading-with-worldbrain-65f244f540b8">Vision</a></li>
-//                   <li className="menu-roadmap"><a href="https://www.notion.so/worldbrain/Release-Notes-Roadmap-262a367f7a2a48ff8115d2c71f700c14">Roadmap</a></li>
-//                   <li className="menu-team"><a href="https://www.notion.so/worldbrain/TEAM-9ca5429f6a5c480289386a5cb49f173c">Team</a></li>
-//                 </ul>
-//               </li>
-//               <li className="menu-github"><a href="https://github.com/WorldBrain">GitHub</a></li>
-//               <li className="menu-upgrade"><Link to="/pricing">Upgrade</Link></li>
-//               {user ? (
-//                 <li className="dropdown menu-about"><a className="dropdown-toggle">Account</a>
-//                   <ul className="dropdown-menu">
-//                     <li><Link to="/profile">Account Data</Link></li>
-//                     <li><Link to="/subscription">Subscriptions</Link></li>
-//                     <li><a onClick={this.handleLogOut} href="#">Log out</a></li>
-//                   </ul>
-//                 </li>
-//               )
-//                 :
-//                 <li className="menu-login"><Link to="/login">Sign In / Sign Up</Link></li>}
-//             </ul>
-//           </nav>
-//         </div>
-//       </header>
-//     )
-//   }
-// }
+    return (
+      <header className={HeaderClass} role="banner">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span> </button>
+            <div id="logo">
+              <Link to="/">
+                <img width="164" height="40" src="/images/logo_horizontal_small-164x40.png" alt="Memex by worldbrain.io" />
+              </Link>
+            </div>
+          </div>
+          <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+            <ul id="menu-main-menu-bar" className="nav navbar-nav">
+              <li className="dropdown menu-about"><a className="dropdown-toggle">About</a>
+                <ul className="dropdown-menu">
+                  <li className="menu-jobs"><a href="https://www.notion.so/worldbrain/4d79e20aa01d4b9e8b8c12d88bb1b4ad">Jobs</a></li>
+                  <li className="menu-faqs"><a href="/faq">FAQs</a></li>
+                  <li className="menu-vision"><a href="https://medium.com/@WorldBrain/where-we-are-heading-with-worldbrain-65f244f540b8">Vision</a></li>
+                  <li className="menu-roadmap"><a href="https://www.notion.so/worldbrain/Release-Notes-Roadmap-262a367f7a2a48ff8115d2c71f700c14">Roadmap</a></li>
+                  <li className="menu-team"><a href="https://www.notion.so/worldbrain/TEAM-9ca5429f6a5c480289386a5cb49f173c">Team</a></li>
+                </ul>
+              </li>
+              <li className="menu-github"><a href="https://github.com/WorldBrain">GitHub</a></li>
+              <li className="menu-upgrade"><Link to="/pricing">Upgrade</Link></li>
+              {user ? (
+                <li className="dropdown menu-about"><a className="dropdown-toggle">Account</a>
+                  <ul className="dropdown-menu">
+                    <li><Link to="/profile">Account Data</Link></li>
+                    <li><Link to="/subscription">Subscriptions</Link></li>
+                    <li><a onClick={this.handleLogOut} href="#">Log out</a></li>
+                  </ul>
+                </li>
+              )
+                :
+                <li className="menu-login"><Link to="/login">Sign In / Sign Up</Link></li>}
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )
+  }
+}
